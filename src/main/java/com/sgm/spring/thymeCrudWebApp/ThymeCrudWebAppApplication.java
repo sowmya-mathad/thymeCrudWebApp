a10c9d50edc143ba57657a5eb1e248a8b3eff13a
Sowmya Girish <36289569+sowmya-mathad@users.noreply.github.com>
2024-07-11 10:20:05 -0400

    Initial commit

---
+++ src/main/java/com/sgm/spring/thymeCrudWebApp/ThymeCrudWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.sgm.spring.thymeCrudWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeCrudWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeCrudWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgm/spring/thymeCrudWebApp/ThymeCrudWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgm.spring.thymeCrudWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeCrudWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
